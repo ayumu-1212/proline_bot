e8dd35f08e171fae750cd017bf529e791acd7e94
YazawaNaoki <yazawa.naoki.infratop@gmail.com>
2020-03-26 18:50:48 +0900

    add push

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -10,6 +10,18 @@ module Api
         else
           render json: { status: 'ERROR', message: 'Message not saved.', data: message.errors }, status: :unprocessable_entity
         end
+
+        message = {
+          type: 'text',
+          text: '朝になりました。本日も頑張りましょう。食べた食べ物を「ひらがな」で入力すると、食品のカロリーと本日のトータルカロリーが表示されます。入力ミスの場合、「みす」と入力すると最新の入力を消去できます。カロリー計算に使ってください。'
+        }
+        client = Line::Bot::Client.new { |config|
+          config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+          config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+        }
+        response = client.push_message(ENV["LINE_CHANNEL_USER_ID"], message)
+        p response
+
       end
 
       private
