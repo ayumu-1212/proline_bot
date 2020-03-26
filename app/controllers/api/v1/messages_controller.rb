ff2ea75981fe486e19d261db3fcdab8f1225fb8b
YazawaNaoki <yazawa.naoki.infratop@gmail.com>
2020-03-26 19:08:24 +0900

    add push

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,14 +3,6 @@ module Api
     class MessagesController < ApplicationController
       protect_from_forgery
       def create
-        message = Message.new(message_params)
-
-        if message.save
-          render json: { status: 'SUCCESS', message: 'Saved message.', data: message }, status: :ok
-        else
-          render json: { status: 'ERROR', message: 'Message not saved.', data: message.errors }, status: :unprocessable_entity
-        end
-
         message = {
           type: 'text',
           text: '朝になりました。本日も頑張りましょう。食べた食べ物を「ひらがな」で入力すると、食品のカロリーと本日のトータルカロリーが表示されます。入力ミスの場合、「みす」と入力すると最新の入力を消去できます。カロリー計算に使ってください。'
@@ -19,8 +11,17 @@ module Api
           config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
           config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
         }
-        response = client.push_message(ENV["LINE_CHANNEL_USER_ID"], message)
+        response = client.push_message("<to>", message)
         p response
+
+        message = Message.new(message_params)
+
+        if message.save
+          render json: { status: 'SUCCESS', message: 'Saved message.', data: message }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Message not saved.', data: message.errors }, status: :unprocessable_entity
+        end
+
 
       end
 
